b8e6cc0f25ccf13397cc454474dc31a043460c68
queenalpha <oxcroprichvie@gmail.com>
2024-09-09 02:01:01 +0700

    new view

--- script copy.js
+++ script copy.js
@@ -1,48 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var countdownElement = document.getElementById('countdown');
-    var countdownLabel = document.getElementById('countdownLabel');
-    var clickButton = document.getElementById('clickButton');
-    var pinInput = document.getElementById('pinInput');
-    var submitPin = document.getElementById('submitPin');
-    var myModal = new bootstrap.Modal(document.getElementById('myModal'));
+// document.addEventListener('DOMContentLoaded', function() {
+//     var countdownElement = document.getElementById('countdown');
+//     var countdownLabel = document.getElementById('countdownLabel');
+//     var clickButton = document.getElementById('clickButton');
+//     var pinInput = document.getElementById('pinInput');
+//     var submitPin = document.getElementById('submitPin');
+//     var myModal = new bootstrap.Modal(document.getElementById('myModal'));
 
-    var endDate = new Date('2024-09-02T21:30:00'); 
+//     var endDate = new Date('2024-09-02T21:30:00'); 
 
-    function updateCountdown() {
-        var currentTime = new Date().getTime();
-        var endTime = endDate.getTime();
-        var timeLeft = endTime - currentTime;
+//     function updateCountdown() {
+//         var currentTime = new Date().getTime();
+//         var endTime = endDate.getTime();
+//         var timeLeft = endTime - currentTime;
 
-        if (timeLeft <= 0) {
-            clearInterval(countdownTimer);
-            countdownElement.style.display = 'none';
-            countdownLabel.style.display = 'none';
-            clickButton.classList.remove('d-none'); 
-        } else {
-            var days = Math.floor(timeLeft / (24 * 60 * 60 * 1000));
-            var hours = Math.floor((timeLeft % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
-            var minutes = Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000));
-            var seconds = Math.floor((timeLeft % (60 * 1000)) / 1000);
-            countdownElement.textContent =  days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + (seconds < 10 ? '0' : '') + seconds + ' second';
-        }
-    }
+//         if (timeLeft <= 0) {
+//             clearInterval(countdownTimer);
+//             countdownElement.style.display = 'none';
+//             countdownLabel.style.display = 'none';
+//             clickButton.classList.remove('d-none'); 
+//         } else {
+//             var days = Math.floor(timeLeft / (24 * 60 * 60 * 1000));
+//             var hours = Math.floor((timeLeft % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+//             var minutes = Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000));
+//             var seconds = Math.floor((timeLeft % (60 * 1000)) / 1000);
+//             countdownElement.textContent =  days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + (seconds < 10 ? '0' : '') + seconds + ' second';
+//         }
+//     }
 
-    var countdownTimer = setInterval(updateCountdown, 1000);
+//     var countdownTimer = setInterval(updateCountdown, 1000);
 
-    clickButton.addEventListener('click', function() {
-        myModal.show(); 
-    });
+//     clickButton.addEventListener('click', function() {
+//         myModal.show(); 
+//     });
 
-    submitPin.addEventListener('click', function() {
-        var pin = pinInput.value;
-        if (pin === '02022024') {
-            window.location.href = 'intersweet by fahri.pdf'; 
-        } else {
-            alert('Yah, salah. Masa gak tau sih, coba tanya temanmu');
-        }
-    });
+//     submitPin.addEventListener('click', function() {
+//         var pin = pinInput.value;
+//         if (pin === '02022024') {
+//             window.location.href = 'intersweet by fahri.pdf'; 
+//         } else {
+//             alert('Yah, salah. Masa gak tau sih, coba tanya temanmu');
+//         }
+//     });
 
-    updateCountdown();
-});
+//     updateCountdown();
+// });
 
 
--- script.js
+++ script.js
@@ -1,42 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var _0x2fdb35 = document.getElementById("countdown");
-    var _0x106bf9 = document.getElementById('countdownLabel');
-    var _0x367382 = document.getElementById('clickButton');
-    var _0x19f55a = document.getElementById("pinInput");
-    var _0x911865 = document.getElementById('submitPin');
-    var _0x5e4cdb = new bootstrap.Modal(document.getElementById("myModal"));
-    var _0x327487 = new Date('2024-09-03T10:15:00');
-    function _0x321a4b() {
-      var _0x10a6fa = new Date().getTime();
-      var _0x51b560 = _0x327487.getTime();
-      var _0x39725f = _0x51b560 - _0x10a6fa;
-      if (_0x39725f <= 0x0) {
-        clearInterval(_0x44d301);
-        _0x2fdb35.style.display = 'none';
-        _0x106bf9.style.display = "none";
-        _0x367382.classList.remove("d-none");
-      } else {
-        var _0x3db9e4 = Math.floor(_0x39725f / 86400000);
-        var _0x21b75a = Math.floor(_0x39725f % 86400000 / 3600000);
-        var _0x489656 = Math.floor(_0x39725f % 3600000 / 60000);
-        var _0x113a0a = Math.floor(_0x39725f % 60000 / 0x3e8);
-        document.getElementById("days").textContent = _0x3db9e4;
-        document.getElementById("hours").textContent = _0x21b75a < 0xa ? '0' + _0x21b75a : _0x21b75a;
-        document.getElementById("minutes").textContent = _0x489656 < 0xa ? '0' + _0x489656 : _0x489656;
-        document.getElementById('seconds').textContent = _0x113a0a < 0xa ? '0' + _0x113a0a : _0x113a0a;
-      }
-    }
-    var _0x44d301 = setInterval(_0x321a4b, 0x3e8);
-    _0x367382.addEventListener("click", function () {
-      _0x5e4cdb.show();
-    });
-    _0x911865.addEventListener("click", function () {
-      var _0x34835f = _0x19f55a.value;
-      if (_0x34835f === "21061961") {
-        window.location.href = 'internsweetLetter.pdf';
-      } else {
-        alert("Yah, salah. Masa gak tau sih, coba cari atau kalo masih gabisa tanya ADMINNYA YA");
-      }
-    });
-    _0x321a4b();
-  });
+const names = ["Barchetta", "Rina", "Reyhan", "Zackwan", "Farabi", "Seno", "Caca", 
+    "Fahri", "Zahra", "Raina", "Mei",  "Tian",  "Hurin","Hauzan", "Arya", "Eber", "Audy", "Rham", "Azel", "Dandi"];
+
+let index = 0;
+
+// Fungsi untuk mengganti teks
+function changeGreeting() {
+const greetingElement = document.getElementById('greeting');
+greetingElement.textContent = `Hi, ${names[index]}`;
+index = (index + 1) % names.length; 
+}
+
+setInterval(changeGreeting, 1000);

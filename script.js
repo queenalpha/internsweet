5aa2bca9dcee19509c9ad3dfa2f0ee0f9491cf43
queenalpha <oxcroprichvie@gmail.com>
2024-09-10 14:36:36 +0700

    update greeting dan memories

--- script.js
+++ script.js
@@ -11,33 +11,3 @@ index = (index + 1) % names.length;
 
 setInterval(changeGreeting, 1000);
 
-document.addEventListener('DOMContentLoaded', function() {
-    var countdownElement = document.getElementById('timer');
-    var mainContentElement = document.getElementById('mainContent');
-    
-    var endDate = new Date('2024-09-09T15:00:00');
-
-    function updateCountdown() {
-        var currentTime = new Date().getTime();
-        var endTime = endDate.getTime();
-        var timeLeft = endTime - currentTime;
-
-        if (timeLeft <= 0) {
-            clearInterval(countdownTimer);
-            countdownElement.remove(); 
-            mainContentElement.classList.remove('hidden');
-        } else {
-            var hours = Math.floor(timeLeft / (60 * 60 * 1000));
-            var minutes = Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000));
-            var seconds = Math.floor((timeLeft % (60 * 1000)) / 1000);
-
-            document.getElementById('hours').textContent = hours;
-            document.getElementById('minutes').textContent = minutes;
-            document.getElementById('seconds').textContent = (seconds < 10 ? '0' : '') + seconds;
-        }
-    }
-
-    var countdownTimer = setInterval(updateCountdown, 1000);
-
-    updateCountdown();
-});
